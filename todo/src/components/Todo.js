8104d89ef8def843a3e4e47fc735739671b990d6
AJLambda <apj2277@gmail.com>
2019-04-08 15:26:49 -0700

    adding comments

--- todo/src/components/Todo.js
+++ todo/src/components/Todo.js
@@ -1,4 +1,8 @@
+//This component will be "connected" to the Redux Store
+
 import React from "react";
+
+//Step 1- import connect from react-redux
 import { connect } from "react-redux";
 
 import { addTodo, toggleCompleted } from "../actions";
@@ -41,12 +45,24 @@ class Todo extends React.Component {
   }
 }
 
+//Step 3- create mapStatetoProps
+
+//mapStatetoProps is a function that tells connect which pieces of state to pass to the component
+//Takes state as an argument (the entire state tree, whatever your reducer is returning as the state tree)
+//Returns an object. The properties in the returned object are what get passed to the component as props.
+//The values in the returned object are properties from the state object (state tree)
 const mapStatetoProps = state => {
   return {
     todos: state.todos
   };
 };
 
+//Step 2- instead of exporting the component, export the connect function call
+//connect is a HOF
+
+//Invoke connect twice-
+//First call, pass in a function and an object
+//Second call, pass in the component that we are connecting
 export default connect(
   mapStatetoProps,
   { addTodo, toggleCompleted }
