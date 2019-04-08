fba3e259e1374d46796bd94714f7867cebb5e9d4
AJLambda <apj2277@gmail.com>
2019-04-08 13:38:49 -0700

    working on reducer

---
+++ todo/src/reducers/index.js
@@ -0,0 +1,24 @@
+const initialState = {
+  todos: [
+    {
+      todo: "Walk the Dog",
+      id: 1452,
+      completed: false
+    },
+    {
+      todo: "Brush Teeth",
+      id: 1356,
+      completed: false
+    },
+    {
+      todo: "Take a Shower",
+      id: 8273,
+      completed: false
+    }
+  ]
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+  }
+};
